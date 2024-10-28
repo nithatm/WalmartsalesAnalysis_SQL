a2507ad2e5e682a5825119e9ddcf9bd9b21cffa8
Nitha T M <159911612+nithatm@users.noreply.github.com>
2024-10-28 17:05:45 +0530

    Delete nnnn.sql

--- nnnn.sql
+++
@@ -1,7 +0,0 @@
--- which branch sold more products than average product sold?--
-
-select Branch,
-sum(Quantity)
-from salesdata1
-group by Branch
-having sum(quantity)> (select avg(quantity) from salesdata1);
